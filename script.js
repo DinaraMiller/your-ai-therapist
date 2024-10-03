7c51170a92b7b9b517425f0c3fe2ac21e3447410
DinaraMiller <diluv87@gmail.com>
2024-10-03 17:26:41 -0500

    Added basic form handling and Typewriter pludin

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function advisor(event) {
+  event.preventDefault();
 
+  new Typewriter("#advice", {
+    strings:
+      "You can't control the situation but you can control your reaction...",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let adviceElement = document.querySelector("#advice-generator-form");
+adviceElement.addEventListener("submit", advisor);
